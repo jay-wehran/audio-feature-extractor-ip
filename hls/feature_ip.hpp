79432c806b012a45498050106047aea3c6368d73
Jason Wehran <jaywehran@gmail.com>
2026-04-08 20:53:55 -0400

    Add energy accumulator and zcr accumulator proof of concepts

---
+++ hls/feature_ip.hpp
@@ -0,0 +1,26 @@
+/*
+ * author: Jason Wehran
+*/
+
+#ifndef FEATURE_IP_HPP
+#define FEATURE_IP_HPP
+
+#include <cstdint>
+#include <cstdio>
+#include <vector>
+
+
+struct FeaturePacket {
+    int frame_id;
+    uint64_t energy;
+    int zcr;
+};
+
+
+int64_t extract_energy(int16_t sample);
+
+bool detect_zcr(int16_t previous_sample, int16_t current_sample);
+
+
+
+#endif // FEATURE_IP_HPP
